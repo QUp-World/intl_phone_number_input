c48c069c2ad0d13f64bdbae7c5046e2d5683393e
Nhuan Vu <nhuanvd@live.com>
2021-04-15 08:07:18 +0700

    Remove check phone number valid when set initial phone number

--- lib/src/widgets/input_widget.dart
+++ lib/src/widgets/input_widget.dart
@@ -170,10 +170,12 @@ class _InputWidgetState extends State<InternationalPhoneNumberInput> {
   void initialiseWidget() async {
     if (widget.initialValue != null) {
       if (widget.initialValue.phoneNumber != null &&
-          widget.initialValue.phoneNumber.isNotEmpty &&
+              widget.initialValue.phoneNumber.isNotEmpty
+          /* &&
           await PhoneNumberUtil.isValidNumber(
               phoneNumber: widget.initialValue.phoneNumber,
-              isoCode: widget.initialValue.isoCode)) {
+              isoCode: widget.initialValue.isoCode)*/
+          ) {
         controller.text =
             await PhoneNumber.getParsableNumber(widget.initialValue);
 
